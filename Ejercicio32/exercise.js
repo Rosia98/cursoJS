82bbb8048730fb133d77293e058d3b73a44e749c
Rosia98 <99267572+Rosia98@users.noreply.github.com>
2024-01-17 18:28:30 +0100

    Add files via upload

--- Ejercicio32/exercise.js
+++ Ejercicio32/exercise.js
@@ -1,15 +1,10 @@
-const person = {
-  id: 1,
-  firstName: 'Mario',
-  lastName: 'Rossi',
-  age: 25,
-};
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
 
-const filtrarPerson = {
-  id: person.id,
-  age: person.age
-};
-
-const json = JSON.stringify(filtrarPerson);
-
-console.log(json); // Should return: { id: 1, age: 25 }
+const developer = new Person("Mario", "Rossi");
+//console.log(developer.firstName + " " + developer.lastName);
+console.log(`${desarrollador.primerNombre} ${desarrollador.apellido}`);
